b70d297d0e56d78146d60e2044c29cfc21b97ef9
Barton Willis <willisb@unk.edu>
2021-03-22 08:21:47 -0500

    experimental function fake-muln
    popping a constant from a product doesn't require a full simplification of the product.

--- qm_momentum_and_position_op.lisp
+++ qm_momentum_and_position_op.lisp
@@ -1,6 +1,6 @@
 
 ;;; Declare the reduced Planck constant to be constant. That makes ħ outative for a 
-;; linear operator.
+;;; linear operator.
 
 (mfuncall '$declare '$ħ '$constant)
 (mfuncall '$declare '|$p| '$operator)
@@ -21,6 +21,9 @@
 (mfuncall '$put '|$q|  #'(lambda (s) (mul '$x s)) '$formula)
 (mfuncall '$put '|$p|  #'(lambda (s) (mul -1 '$%i '$ħ ($diff s '$x))) '$formula)
 
+(defun fake-muln (e)
+    (if (cdr e) (cons '(mtimes simp) e) (car e)))
+
 (defun simp-momentum-op (e y z)
     (declare (ignore y))
     (oneargcheck e)
@@ -31,7 +34,7 @@
           ((mplusp e)
              (addn (mapcar #'(lambda (s) (take '(|$p|) s)) (cdr e)) t))
           ((and (mtimesp e) ($constantp (second e)))
-             (mult (second e) (take '(|$p|) (muln (cddr e) t))))
+             (mult (second e) (take '(|$p|) (fake-muln (cddr e)))))
           (t (list (list '|$p| 'simp) e))))) 
 
 (setf (get '|$p| 'operators) #'simp-momentum-op)
@@ -43,7 +46,7 @@
       (cond ((mplusp e)
              (addn (mapcar #'(lambda (s) (take '(|$q|) s)) (cdr e)) t))
           ((and (mtimesp e) ($constantp (second e)))
-             (mult (second e) (take '(|$q|) (muln (cddr e) t))))
+             (mult (second e) (take '(|$q|) (fake-muln (cddr e)))))
           (t (list (list '|$q| 'simp) e))))) 
 
 (setf (get '|$q| 'operators) #'simp-position-op)    
